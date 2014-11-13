38e741f6cc1d7dacdac48da43638b3208520734e
jovchi <jovchi@yahoo.com>
2014-11-13 07:50:04 -0500

    Update: documentation

--- PrimeClient.py
+++ PrimeClient.py
@@ -13,22 +13,23 @@ import rpyc
 import ipaddress
 
 #ask Client the location/server of the Service he wish to access
-IPAddress= input(" What is the location(IP) of the server you wish to access?")
-
-empty_str=""
+IPAddress= input(" What is the location(IP) of the server you wish to access?: ")
 
 #Validate IP address
 try:
  ipaddress.ip_address(IPAddress)
 
 except ValueError:
-	IPAddress=input("Sorry, wrong input. Please input correct location(IP) of the server you wish to access?")
+	IPAddress=input("Sorry, wrong input. Please input correct location(IP) of the server you wish to access?: ")
 
 # connect to server
 c=rypc.connect(IPAddress,12345) 
 
-
-num =int( input("What number would you like to check if prime?))
+# validate input is a number
+try:
+	num=int(input("What number would you like to check if prime?: " ))
+except ValueError:
+    num = int(input("Sorry, wrong input. Please input a number: "))
 
 print(c.root.is_prime(num))
 
